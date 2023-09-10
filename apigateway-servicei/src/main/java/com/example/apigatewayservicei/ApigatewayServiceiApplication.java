9d21af80a00f1601fac74a3b86c219644ee12a90
seok862 <tjrwn1247@gmail.com>
2023-09-10 18:00:18 +0900

    init

---
+++ apigateway-servicei/src/main/java/com/example/apigatewayservicei/ApigatewayServiceiApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayservicei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServiceiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServiceiApplication.class, args);
+    }
+
+}
